c52d3f9eceac6e3d3293643157f8bfff0c2624ec
Aditya Kumar Sharma <aditya.sharma2@shipsy.co.in>
2022-01-22 19:28:19 +0530

    imdb-task : added indexes in db and added MovieAdmin and GenreAdmin"

--- movies/api/models.py
+++ movies/api/models.py
@@ -6,7 +6,7 @@ class Genre(models.Model):
     """
     Genre model to store all types of Genre
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True)
 
     def __str__(self):
         return self.name
@@ -17,9 +17,9 @@ class Movie(models.Model):
     Movie model to store movies data
     """
     popularity = models.FloatField()
-    director = models.CharField(max_length=250)
+    director = models.CharField(max_length=250, db_index=True)
     imdb_score = models.FloatField()
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, db_index=True)
     genre = models.ManyToManyField(Genre)
 
     def __str__(self):
